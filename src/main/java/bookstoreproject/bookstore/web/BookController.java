81974947babd0dc7ebbb27164af58e5ccf8bf469
bhi279 <bhi279@myy.haaga-helia.fi>
2024-09-03 12:59:14 +0300

    Initial commit

---
+++ src/main/java/bookstoreproject/bookstore/web/BookController.java
@@ -0,0 +1,14 @@
+package bookstoreproject.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+
+    @GetMapping("/index")
+    public String showIndexPage() {
+        return "index";
+    }
+    
+}
