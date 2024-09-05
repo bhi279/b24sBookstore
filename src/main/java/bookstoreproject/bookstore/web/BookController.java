b8474605b8b454528cdb054875fff92db896c121
bhi279 <bhi279@myy.haaga-helia.fi>
2024-09-05 09:31:17 +0300

    Added create and delete functionalities

--- src/main/java/bookstoreproject/bookstore/web/BookController.java
+++ src/main/java/bookstoreproject/bookstore/web/BookController.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BookController {
@@ -24,4 +28,21 @@ public class BookController {
         return "booklist";
     }
 
+    @GetMapping("/add")
+    public String addStudent(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/save")
+    public String save(Book book) {
+        bookRepository.save(book);
+        return "redirect:booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id, Model model) {
+        bookRepository.deleteById(id);
+        return "redirect:../booklist";
+    }
 }
